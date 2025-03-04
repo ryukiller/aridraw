e18e5dd5abe36e137983d1a244cbe087633c3bb8
ryukiller <rian.rodrigo.webmaster@gmail.com>
2025-03-05 00:30:58 +0100

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public', // Where the service worker file will be generated
+    disable: process.env.NODE_ENV === 'development', // Disable PWA features in development
+    ...nextConfig,
+});
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Your imagination at your pocket - AriDraw",
   description: "Unleash your inner artist with AriDraw Creative Canvas a modern, responsive digital painting and drawing app. Experiment with multiple brush types, vibrant color palettes, and custom backgrounds. Save, revisit, and share your creative history with ease.",
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  icons: {
+    icon: '/icons/icon-192x192.png',
+    apple: '/icons/icon-192x192.png',
+  },
 };
 
 export default function RootLayout({
